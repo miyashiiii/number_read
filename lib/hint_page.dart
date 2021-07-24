080c83c1eebfd0e75ddbac502f13463e85b727bb
miyashiiii <yosuke.miyashita@gmail.com>
2021-07-24 11:00:13 +0900

    refactor: Use ListView on hint page

--- lib/hint_page.dart
+++ lib/hint_page.dart
@@ -2,8 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HintPage extends StatelessWidget {
-  List<Widget> hintTable(context) {
-    List<Widget> list = [];
+  Divider createDivider(index) {
+    double dividerThickness = 1;
+    Color dividerColor = Colors.black12;
+    if (index % 4 == 0) {
+      dividerThickness = 2;
+      dividerColor = Colors.black26;
+    }
+    return Divider(
+      height: 4.h,
+      indent: 100.h,
+      endIndent: 100.h,
+      thickness: dividerThickness,
+      color: dividerColor,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     Map map = {
       '読み': '数字',
       '1': '1',
@@ -19,87 +35,68 @@ class HintPage extends StatelessWidget {
       '100億': '10,000,000,000',
       '1000億': '100,000,000,000'
     };
-    var count = 0;
-    var isHeader = true;
-    map.forEach((var key, var value) {
-      var topMargin = 10;
-      double dividerThickness = 1;
-      Color dividerColor = Colors.black12;
-      if (count % 4 == 1) {
-        topMargin = 20;
-      } else if (count % 4 == 0) {
-        dividerThickness = 2;
-        dividerColor = Colors.black26;
-      }
-      count++;
-      list.addAll([
-        SizedBox(
-          height: topMargin.h,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-                alignment: Alignment.centerRight,
-                width: 60,
-                child: Text(
-                  key,
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                )),
-            Container(
-                alignment: Alignment.center,
-                width: 60,
-                child: Text(isHeader ? "" : "→")),
-            Container(
-                alignment: Alignment.centerRight,
-                width: 120,
-                child: Text(
-                  value,
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                )),
-          ],
-        ),
-        Divider(
-          thickness: dividerThickness,
-          color: dividerColor,
-          indent: 60.h,
-          endIndent: 60.h,
-        )
-      ]);
-      isHeader = false;
-    });
-    list.addAll([
-      SizedBox(height: 10),
-      ElevatedButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text('戻る'),
-      )
-    ]);
-    return list;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ヒント'),
-        centerTitle: true,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            children: hintTable(context),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('ヒント'),
           ),
-        ),
-      ),
+          body: Column(children: [
+            Expanded(
+                child: ListView.separated(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: map.length + 1,
+                    separatorBuilder: (context, index) {
+                      return createDivider(index);
+                    },
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index == map.length) {
+                        return createDivider(index);
+                      }
+                      bool isHeader = index == 0;
+                      String key = map.keys.elementAt(index);
+                      var topMargin = 0.h;
+                      if (index % 4 == 1) {
+                        topMargin = 30.h;
+                      }
+                      return Container(
+                          height: 100.h+topMargin,
+                          padding: EdgeInsets.only(top:topMargin),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                  alignment: Alignment.centerRight,
+                                  width: 60,
+                                  child: Text(
+                                    key,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  )),
+                              Container(
+                                  alignment: Alignment.center,
+                                  width: 60,
+                                  child: Text(isHeader ? "" : "→")),
+                              Container(
+                                  alignment: Alignment.centerRight,
+                                  width: 120,
+                                  child: Text(
+                                    map[key],
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  )),
+                            ],
+                          ));
+                    })),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('戻る'),
+            ),
+            SizedBox(height: 150.h),
+          ])),
     );
   }
 }
